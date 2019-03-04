a1507e7dc2635ee575ff6d3c9775dc2f99ab7aa5
MyNameZMD <1909781417@qq.com>
2019-03-04 15:36:53 +0800

    Initial commit

---
+++ src/main/java/com/jpbook/JpbookApplication.java
@@ -0,0 +1,13 @@
+package com.jpbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpbookApplication.class, args);
+	}
+
+}
